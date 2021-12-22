f2ffd9046b0d54d4d7277768f39e1b8841597410
AAlkandari <96235631+AAlkandari@users.noreply.github.com>
2021-12-22 11:25:42 +0300

    DONE

--- index.js
+++ index.js
@@ -8,4 +8,64 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
-}
+
+  height = height/100
+
+    const BMI = (weight/(height*height))
+
+      if(age <19)
+    {
+  if(BMI<19  )
+  alert(`your BMI is ${BMI} & you are underweight`)
+  else if(BMI <24)
+  alert(`your BMI is ${BMI} & you are healthy`)
+  if(BMI>24) alert(`your BMI is ${BMI} & you are overweight`)
+    }
+
+     else if(age <=34)
+    {
+  if(BMI<20 )
+  alert(`your BMI is ${BMI} & you are underweight`)
+  else if(BMI <25)
+  alert(`your BMI is ${BMI} & you are healthy`)
+  else alert(`your BMI is ${BMI} & you are overweight`)
+   }
+  
+    else if(age <=44)
+  {
+  if(BMI<21 )
+  alert(`your BMI is ${BMI} & you are underweight`)
+  else if(BMI <26)
+  alert(`your BMI is ${BMI} & you are healthy`)
+  else alert(`your BMI is ${BMI} & you are overweight`)
+  }
+
+    else if(age <=54)
+  {
+  if(BMI<22 )
+  alert(`your BMI is ${BMI} & you are underweight`)
+  else if(BMI <27)
+  alert(`your BMI is ${BMI} & you are healthy`)
+  else alert(`your BMI is ${BMI} & you are overweight`)
+  }
+
+    else if(age <=64)
+  {
+  if(BMI<23 )
+  alert(`your BMI is ${BMI} & you are underweight`)
+  else if(BMI <28)
+  alert(`your BMI is ${BMI} & you are healthy`)
+  else alert(`your BMI is ${BMI} & you are overweight`)
+  }
+
+  else if(age >65)
+  {
+  if(bmi<24 )
+  alert(`your BMI is ${bmi} & you are underweight`)
+  else if(bmi <29)
+  alert(`your BMI is ${bmi} & you are healthy`)
+  else alert(`your BMI is ${bmi} & you are overweight`)
+  }
+  
+  
+  }
